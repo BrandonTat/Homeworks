bdf153758927a117bb5a290869a1cab0f0eb1126
Brandon Tat <brandontat7@gmail.com>
2017-03-30 22:00:58 -0700

    Add assign(key, value) to Map class

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -3,7 +3,20 @@ class Map
     @map = []
   end
 
+  def assign(key, value)
+    @map << [key, value]
+  end
+
   def show
     p @map
   end
 end
+
+m = Map.new
+m.show
+
+m.assign(1, 10)
+m.show
+
+m.assign(2, 10)
+m.show
