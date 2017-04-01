1023c8ecd23ef56c284995e4a5a39cebe07d71e6
Brandon Tat <brandontat7@gmail.com>
2017-04-01 15:21:59 -0700

    Completed

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -10,7 +10,12 @@ class Simon
   end
 
   def play
+    until @game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
@@ -28,7 +33,15 @@ class Simon
   end
 
   def require_sequence
+    puts "Please input the sequence you just saw one color at a time"
+    answer = gets.chomp
 
+    @seq.each do |color|
+      if color != answer
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -36,7 +49,8 @@ class Simon
   end
 
   def round_success_message
-
+    puts "Correct"
+    puts "Here's the next sequence:"
   end
 
   def game_over_message
@@ -49,3 +63,11 @@ class Simon
     @seq = []
   end
 end
+
+
+# The specs should guide you as to what each method does.
+# One important thing to note is that it is generally impractical to test
+# console user input, so the specs dont test require_sequence.
+# The goal of require_sequence is to prompt the user to repeat back the
+# new sequence that was just shown to them, one color at a time. If
+# they give an incorrect color, it should immediately set @game_over to be true.
