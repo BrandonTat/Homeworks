17350a5185d8a1f8463b74761188f6e6cfa5d6f5
Brandon Tat <brandontat7@gmail.com>
2017-04-06 00:03:33 -0700

    Add slow_dance method and tests

--- W2D4/dancing_octopus.rb
+++ W2D4/dancing_octopus.rb
@@ -1,2 +1,12 @@
 tiles_array = ["up", "right-up", "right", "right-down",
                "down", "left-down", "left", "left-up" ]
+
+def slow_dance(direction, array)
+  array.each_with_index do |dir, tentacle|
+    return tentacle if dir == direction
+  end
+end
+
+# Testing slow dance
+p slow_dance("up", tiles_array)
+p slow_dance("right-down", tiles_array)
