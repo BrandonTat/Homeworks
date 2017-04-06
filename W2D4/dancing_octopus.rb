63226af24059d451ae5039053688cc1300a74472
Brandon Tat <brandontat7@gmail.com>
2017-04-06 00:09:53 -0700

    Add fast_dance and tests

--- W2D4/dancing_octopus.rb
+++ W2D4/dancing_octopus.rb
@@ -5,8 +5,24 @@ def slow_dance(direction, array)
   array.each_with_index do |dir, tentacle|
     return tentacle if dir == direction
   end
+
+  nil
+end
+
+tiles_hash = {
+  "up" => 0, "right-up" => 1, "right" => 2,
+  "right-down" => 3, "down" => 4,
+  "left-down" => 5, "left" => 6, "left-up" => 7
+}
+
+def fast_dance(direction, hash)
+  hash.key?(direction) ? hash[direction] : nil
 end
 
 # Testing slow dance
-p slow_dance("up", tiles_array)
-p slow_dance("right-down", tiles_array)
+# p slow_dance("up", tiles_array)
+# p slow_dance("right-down", tiles_array)
+
+# Testing fast dance
+p fast_dance("up", tiles_hash)
+p fast_dance("right-down", tiles_hash)
