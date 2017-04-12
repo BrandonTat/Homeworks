30e94db8c0349d355c73a474f6afd6d177c8b185
Brandon Tat <brandontat7@gmail.com>
2017-04-11 23:54:13 -0700

    Add validation for House and Person model

--- W3D3/IntroRails/app/models/house.rb
+++ W3D3/IntroRails/app/models/house.rb
@@ -1,3 +1,12 @@
-class House < ActiveRecord::Base
+# == Schema Information
+#
+# Table name: houses
+#
+#  id           :integer     primary key
+#  address      :string
+#  created_at   :datetime
+#  updated_at   :datetime
 
+class House < ActiveRecord::Base
+  validates :address, presence: true
 end
--- W3D3/IntroRails/app/models/person.rb
+++ W3D3/IntroRails/app/models/person.rb
@@ -1,3 +1,14 @@
-class Person < ActiveRecord::Base
+ # == Schema Information
+ #
+ # Table name: people
+ #
+ #  id           :integer     primary key
+ #  name         :string
+ #  house_id     :integer
+ #  created_at   :datetime
+ #  updated_at   :datetime
 
+
+class Person < ActiveRecord::Base
+  validates :name, :house_id, presence: true
 end
